37979b5dcb4c12fdd6dddd8dc1e32ef65a6fd7a9
PrishaSusanne <62793055+PrishaSusanne@users.noreply.github.com>
2020-04-20 15:12:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup(){
+  createCanvas(400, 400);
+  background(200);
+
+  // A rectangle
+rect(200,200,40,120);
+rect(240,210,30,110);
+rect(170,210,30,110);
+
+ellipse(200,200,10,10);
+ellipse(210,200,10,10);
+ellipse(220,200,10,10);
+ellipse(230,200,10,10);
+ellipse(240,200,10,10);
+
+ellipse(240,210,10,10);
+ellipse(250,210,10,10);
+ellipse(260,210,10,10);
+ellipse(270,210,10,10);
+
+ellipse(170,210,10,10);
+ellipse(180,210,10,10);
+ellipse(190,210,10,10);
+ellipse(200,210,10,10);
+}
